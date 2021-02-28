1722df3063e0f35274fc6b5a418e5025528c32d3
TKisely <tamaskisely@gmail.com>
2021-02-28 09:17:52 +0100

    bevezetes updated

--- 1_alk_Bevezetes/bevezetes.cs
+++ 1_alk_Bevezetes/bevezetes.cs
@@ -1,7 +1,28 @@
-class Program
+using System;
+using System.Collections.Generic;
+
+namespace ElsoOra
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello World");
+        static void Main(string[] args)
+        {
+            int elsoSzamunk = 83;
+            double elsoTortem = 83.67;
+            string elsoSzovegem = "ElsoSzovegem";
+
+            Console.WriteLine("Hello World!" + "Valami" + elsoSzovegem);
+            Console.WriteLine("sor");
+            Console.WriteLine("sor \n");
+            Console.WriteLine("sor");
+
+            List<int> lista = new List<int>();
+
+            lista.Add(5);
+            lista.Add(8);
+            lista.Add(11);
+
+            Console.WriteLine(lista[0]);
+        }
     }
 }
